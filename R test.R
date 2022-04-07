0dd877262882ac790faac88b66e82d0437406fdb
arbindchauhan <103194339+arbindchauhan@users.noreply.github.com>
2022-04-07 18:37:04 +0530

    Add files via upload

---
+++ R test.R
@@ -0,0 +1,4 @@
+#2. Write a R program to print the numbers from 1 to 100 and print "Fizz" for multiples
+#of 3, print "Buzz" for multiples of 5, and print "FizzBuzz" for multiples of both
+#1. Write a R program to create a sequence of numbers from 20 to 50 and find the mean
+#of numbers from 20 to 60 and sum of numbers from 51 to 91
